4f4a00e07f03764655c8ab5d6bc007200a0d2da2
Rycho Rantung <55700409+RychoRantung@users.noreply.github.com>
2019-09-26 13:44:41 +0700

    Add files via upload

---
+++ IF1-10118005-Latihan15-Logika/src/if1/pkg10118005/latihan15/logika/IF110118005Latihan15Logika.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118005.latihan15.logika;
+
+/**
+ *
+ * @author USER
+ * NAMA   : Rycho Rantung
+ * KELAS  : IF-1
+ * NIM    : 10118005
+ * Deskripsi Program : Program ini berisi program yang menampilkan logika
+ */
+public class IF110118005Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+        
+    }
+    
+}
